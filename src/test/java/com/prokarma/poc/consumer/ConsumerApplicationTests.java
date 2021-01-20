757de19ddd5efef08fe5f360e8339ca1b3eabc80
bakavar1 <bargavakumar.akavaram1@t-mobile.com>
2021-01-20 11:51:53 +0530

    mysql db

--- src/test/java/com/prokarma/poc/consumer/ConsumerApplicationTests.java
+++ src/test/java/com/prokarma/poc/consumer/ConsumerApplicationTests.java
@@ -3,8 +3,11 @@ package com.prokarma.poc.consumer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(
+        locations = "classpath:application-integrationtest.properties")
 class ConsumerApplicationTests {
 
     @Test
